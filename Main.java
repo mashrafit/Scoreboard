3e23b562fc897b3b20a0feb897ef3f05af89ea69
mashrafit <mashrafit@nycstudents.net>
2024-10-16 15:29:51 +0000

    new

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
 
+        info=game.getScore();
+        System.out.println(info);
+        
+        game.recordPlay(1);
+        info=game.getScore();
+        System.out.println(info);
+        
+        game.recordPlay(0);
+        info=game.getScore();
+        System.out.println(info);
+
+    }
+}
--- Scoreboard.java
+++ Scoreboard.java
@@ -1,1 +1,38 @@
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+    public void recordPlay(int score)
+    {
+        if(teamOne == activeTeam && score > 0){
+            teamOneScore += score;    
+        }
+        else if (teamTwo == activeTeam && score > 0){
+            teamTwoScore += score;
+        }
+        if(score == 0){
+            if (activeTeam == teamOne){
+                activeTeam = teamTwo;
+            } else if(activeTeam == teamTwo) {
+                activeTeam = teamOne;
+            }
+        }
+    
 
+
+
+        }
+        }
